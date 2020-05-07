7c61d97810240a0c52cb354e0ececc3e343d89ec
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-07 22:47:35 +0300

    Update code.py
    Minor change in rules()

--- code.py
+++ code.py
@@ -9,8 +9,15 @@ def title():
 
 def rules():
     print("RULES:")
-    print(" • The game is played on a 3 by 3 square grid.")
+    print(" • The game is played on a 3 by 3 square grid (board).")
     print(" • One player is X and the other is O. The players take turns putting their marks (X or O) in empty squares.")
     print(" • The first player to get 3 of their marks in a row (up, down, across, or diagonally) wins the game.")
     print(" • When all 9 squares are filled, the game is over. If no player has 3 marks in a row, the game ends in a tie.\n")
     # source: https://www.exploratorium.edu/brain_explorer/tictactoe.html
+
+
+board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+]
