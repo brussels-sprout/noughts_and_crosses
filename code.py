14c578aef821ba5872516c74f3c8999c90a39572
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-08 23:28:31 +0300

    Update code.py
    Define example() and intro()

--- code.py
+++ code.py
@@ -2,6 +2,12 @@
 # by brussels_sprout
 
 
+def intro():
+    title()
+    rules()
+    example()
+
+
 def title():
     print("\033[1m" + "Tic Tac Toe" + "\033[0;0m" + "\nby brussels-sprout\n")
     # weird things make it bold
@@ -10,10 +16,22 @@ def title():
 def rules():
     print("Rules:")
     print(" • The game is played on a 3 by 3 square grid (board).")
-    print(" • One player is X and the other is O. The players take turns putting their marks (X or O) in empty squares.")
+    print(" • One player is X and the other is O. The players take turns (X plays first) putting their marks (X or O)"
+          " in empty squares.")
     print(" • The first player to get 3 of their marks in a row (horizontally, vertically, or diagonally) wins the game.")
-    print(" • When all 9 squares are filled, the game is over. If no player has 3 marks in a row, the game ends in a tie.")
+    print(" • When all 9 squares are filled, the game is over. If no player has 3 marks in a row, the game ends in a tie.\n")
     # source: https://www.exploratorium.edu/brain_explorer/tictactoe.html (edited)
+
+
+def example():
+    print("Example:")
+    print("╔════════╤════════╤════════╗     The coordinates of each square are shown on the board.")
+    print("║ (1, 1) │ (1, 2) │ (1, 3) ║")
+    print("╟────────┼────────┼────────╢     To place your mark in a specific square, just input")
+    print("║ (2, 1) │ (2, 2) │ (2, 3) ║     the square's  vertical and horizontal coordinates like this: ")
+    print("╟────────┼────────┼────────╢     1, 3 (example for the top right square).")
+    print("║ (3, 1) │ (3, 2) │ (3, 3) ║")
+    print("╚════════╧════════╧════════╝     Have fun!")
 
 
 def main():
@@ -22,7 +40,7 @@ def main():
         [" ", " ", " "],
         [" ", " ", " "]
     ]
-    print_board(board)
+    # print_board(board)
 
 
 def print_board(b):  # b for board; i promise this looks better during execution ok.
@@ -35,6 +53,5 @@ def print_board(b):  # b for board; i promise this looks better during execution
     print("╚═════╧═════╧═════╝")
 
 
-title()
-rules()
+intro()
 main()
